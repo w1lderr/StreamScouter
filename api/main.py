6aa85ac6824b9fa2667edc26308e041fef4bec17
Zakhar <hxgdjahdbdnsjsbdbsnsbs@gmail.com>
2025-02-19 22:53:41 +0200

    Changed variable name : )

--- api/main.py
+++ api/main.py
@@ -8,10 +8,9 @@ from util import df
 from util import recommend_movie
 from util import display_recommendations
 
-
 app = FastAPI()
 
 @app.get("/getRecommendations")
-async def get_recommendations(movie_name: str):
-    recommendations = recommend_movie(movie_name, model, desc_embeddings, df, top_n=3)
+async def get_recommendations(movie_description: str):
+    recommendations = recommend_movie(movie_description, model, desc_embeddings, df, top_n=3)
     return display_recommendations(recommendations)
